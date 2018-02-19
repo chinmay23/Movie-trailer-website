800e05d1fc36c4fd29623fe013ef1438dc08d798
Chinmay <chinmay2307@gmail.com>
2018-02-19 15:30:38 +0530

    Modified

--- entertainment.py
+++ entertainment.py
@@ -7,19 +7,19 @@ import media
 # list of objects(movies)
 
 dunkirk = media.Movie(
-    "Dunkirk", "In May 1940, Germany advanced into France,
+    "Dunkirk", """In May 1940, Germany advanced into France
     trapping Allied troops on the beaches of Dunkirk.
     At the end of this heroic mission, 330, 000 French,
     British, Belgian and Dutch soldiers were safely
-    evacuated.",
-    "https: // upload.wikimedia.org / wikipedia / en / 1 / 15 /
-    Dunkirk_Film_poster.jpg",
-    "https: // www.youtube.com / watch?v=F - eMt3SrfFU"
+    evacuated.""",
+    """https://en.wikipedia.org/wiki/
+    Dunkirk_(2017_film)#/media/File:Dunkirk_Film_poster.jpg""",
+    "https://www.youtube.com/watch?v=F-eMt3SrfFU"
 )
 
 
 dark_knight_rises = media.Movie(
-    "The Dark Knight Rises", "It has been eight
+    "The Dark Knight Rises", """It has been eight
     years since Batman(Christian Bale),
     in collusion with Commissioner Gordon
     (Gary Oldman), vanished into the night.
@@ -27,49 +27,49 @@ dark_knight_rises = media.Movie(
     burglar(Anne Hathaway) and a merciless
     terrorist named Bane(Tom Hardy) force
     Batman out of exile and into a battle
-    he may not be able to win.",
-    "https: // images - na.ssl - images - amazon.com /
-    images / I / 711IkMGkWtL._SY550_.jpg",
+    he may not be able to win.""",
+    """https://images-na.ssl-images-amazon.com/
+    images/I/711IkMGkWtL._SY550_.jpg""",
     "https://www.youtube.com/watch?v=g8evyE9TuYk"
 )
 
 rush = media.Movie(
-    "Rush", "In the mid - 1970s, charismatic English playboy
+    "Rush", """In the mid - 1970s, charismatic English playboy
     James Hunt(Chris Hemsworth) and Austrian perfectionist
     Niki Lauda(Daniel Brühl) share an intense rivalry in
     Formula 1 racing. Meanwhile, the women(Olivia Wilde)
-    as both drivers risk death with every lap.",
-    "https: // d32qys9a6wm9no.cloudfront.net / images / movies /
-    poster / 08 / 08aee6276db142f4b8ac98fb8ee0ed1b_500x735.jpg?t=1488656061",
-    "https: // www.youtube.com / watch?v=9ZJqxgR - 5Xc"
+    as both drivers risk death with every lap.""",
+    """https://d32qys9a6wm9no.cloudfront.net/images/movies/
+    poster/08/08aee6276db142f4b8ac98fb8ee0ed1b_500x735.jpg?t=1488656061""",
+    "https://www.youtube.com/watch?v=9ZJqxgR-5Xc"
 )
 
 fight_club = media.Movie(
-    "Fight Club", "A depressed man(Edward Norton)
+    "Fight Club", """A depressed man(Edward Norton)
     suffering from insomnia meets a strange soap
     salesman named Tyler Durden(Brad Pitt) and
     soon finds himself living in his squalid house
     after his perfect apartment is destroyed.
     Their perfect partnership frays when Marla
     a fellow support group crasher, attracts
-    Tyler's attention.",
-          "https: // images - na.ssl - images - amazon.com /
-    images / I / 51OsUdPrjoL.jpg",
+    Tyler's attention.""",
+    """https://images-na.ssl-images-amazon.com/
+    images/I/51OsUdPrjoL.jpg""",
     "https://www.youtube.com/watch?v=_XgQA9Ab0Gw"
 )
 
 
 captain_phillips = media.Movie(
-    "Captain Phillips", "In April 2009,
+    "Captain Phillips", """In April 2009,
     the U.S. containership Maersk Alabama
     sails toward its destination on a day
     that seems like any other. The pirates
     are after millions of dollars, and
     Phillips must use his wits to make
     sure everyone survives and returns
-    home safely.",
-    "http: // cdn.collider.com / wp - content /
-    uploads / captain - phillips - poster.jpg",
+    home safely.""",
+    """http://cdn.collider.com/wp-content/
+    uploads/captain-phillips-poster.jpg""",
     "https://www.youtube.com/watch?v=u7JptS2VZNg"
 )
 
