16a4e263a0c85953c293bead10103611b3e0c153
Chinmay <chinmay2307@gmail.com>
2018-02-19 13:28:29 +0530

    Added comments

--- media.py
+++ media.py
@@ -1,15 +1,21 @@
 import webbrowser
 
+# Define Movie class
+
 
 class Movie():
+    # Movie class displays a static webpage displaying list of movies
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer):
-        # Attributes of movies
+        # Reference to current instance of class, accessing the attributes and
+        # methods of class entertainment
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer
+    # Opens webbrowser for playing trailer after clicking on specific movie
+    # poster
 
     def show_trailer(self):
-        # Opens webbrowser for playing trailer
+
         webbrowser.open(self.trailer_youtube_url)
